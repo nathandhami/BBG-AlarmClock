034348b2125a907140154e65a1b44d02945ad2b3
NoorUllah <nra22@sfu.ca>
2017-10-17 15:54:38 -0700

    Add an analog clock and the index page

--- wakeUp/app.js
+++ wakeUp/app.js
@@ -20,10 +20,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
